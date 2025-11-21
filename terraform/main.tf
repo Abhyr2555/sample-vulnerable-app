a095a25e42793c5b67d64b6f17c6e8c7ed9dd6a4
Abhyr2555 <abhayr2555@gmail.com>
2025-11-21 21:58:00 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,47 +9,43 @@
 9: }
 10:
 11: provider "aws" {
-12:   region = "us-east-1" 
+12:   region = "us-east-1"
 13: }
 14:
 15: resource "aws_s3_bucket" "app_bucket" {
 16:   bucket = "sample-app-terraform-bucket-12345"
-17:   acl    = "public-read"                        # Issue 1: public-read ACL 
+17:   # Fix: Removed public-read ACL
 18: }
 19:
-20: # Fixed IAM policy with least privilege
-21: resource "aws_iam_policy" "app_policy" {
-22:   name        = "app-read-only-s3" 
-23:   description = "Allow read-only access to the app S3 bucket"
-24:
-25:   policy = <<EOF
-26: {
-27:   "Version": "2012-10-17",
-28:   "Statement": [
-29:     {
-30:       "Effect": "Allow",
-31:       "Action": [
-32:         "s3:GetObject",  # Added minimal read-only S3 actions
-33:         "s3:ListBucket"
-34:       ],
-35:       "Resource": [
-36:         "arn:aws:s3:::sample-app-terraform-bucket-12345", # Specified bucket ARN
-37:         "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
-38:       ]
-39:     }
-40:   ]
-41: }
-42: EOF
-43: }
-44:
-45: resource "aws_security_group" "open_sg" {
-46:   name        = "open-sg"
-47:   description = "Security group with wide open access"
-48:
-49:   ingress {
-50:     from_port   = 0
-51:     to_port     = 0  
-52:     protocol    = "-1"
-53:     cidr_blocks = ["0.0.0.0/0"]                 # Issue 4: all ports open
-54:   }
-55: }
+20: resource "aws_iam_policy" "app_policy" {
+21:   name        = "app-full-access"
+22:   description = "Policy used by instances"
+23:
+24:   policy = <<EOF
+25: {
+26:   "Version": "2012-10-17",
+27:   "Statement": [
+28:     {
+29:       "Effect": "Allow",
+30:       # Fix: Specified minimum required actions instead of wildcard
+31:       "Action": ["s3:GetObject", "s3:PutObject"],  
+32:       # Fix: Specified bucket resource instead of wildcard
+33:       "Resource": "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
+34:     }
+35:   ]
+36: }
+37: EOF
+38: }
+39:
+40: resource "aws_security_group" "open_sg" {
+41:   name        = "open-sg"
+42:   description = "Security group with wide open access"
+43:
+44:   # Fix: Restricted ingress to only required ports/IPs
+45:   ingress {
+46:     from_port   = 80 
+47:     to_port     = 80
+48:     protocol    = "tcp"
+49:     cidr_blocks = ["10.0.0.0/24"] # Example private subnet
+50:   }
+51: }
